44c18aec9ee8ff16c45fcb084e4fcd24ecf7fb8b
Daniel Vergara <danielvsuarez01@gmail.com>
2020-04-04 22:47:55 -0500

    empezando con jsx

--- src/index.js
+++ src/index.js
@@ -8,9 +8,24 @@ container.appendChild(element) */
 import React from  'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello Platzi Badges from react!</h1>
+// const element = <h1>Hello Platzi Badges from react!</h1>
 
+// /* const name = 'Mile'
+// const sum = ()=> 3+3 */
+/* const element = React.createElement(
+    'h1',
+    {},
+    `Hola ${name}`
+) */
+// const jsx = <h1>Hola soy {null}</h1>
+
+const jsx = (
+    <div>
+        <h1>Hola soy Daniel</h1>
+        <p>Soy ingeniero</p>
+    </div>
+)
 const container = document.getElementById('app')
 
 // ReactDOM.render(que, donde)
-ReactDOM.render(element, container);
+ReactDOM.render(jsx, container);
